543a650d0b5c551bd78925e5892d3f290ed92ee5
Alexandr Melnikov <sasha.mel.aa@gmail.com>
2021-03-10 16:59:26 +0300

    create burger menu for project

--- shared.js
+++ shared.js
@@ -1,45 +1,21 @@
 var backdrop = document.querySelector(".backdrop");
-var modal = document.querySelector(".modal");
-var modalNoButton = document.querySelector(".modal__action--negative");
-var selectPlanButtons = document.querySelectorAll(".plan button");
-// var toggleButton = document.querySelector(".toggle-button");
-var toggleButton = document.querySelector(".header-burger");
+var toggleButton = document.querySelector(".main-burger");
 var mobileNav = document.querySelector(".mobile-nav");
+// var body = document.querySelector("body")
 
 backdrop.addEventListener("click", function() {
   mobileNav.classList.remove("open");
   mobileNav.getElementsByClassName('open')
-  closeModal();
-});
-
-backdrop.addEventListener("click", function() {
   toggleButton.classList.remove("open");
   toggleButton.getElementsByClassName('open')
-  closeModal();
+  // body.classList.remove("lock")
+  // body.getElementsByClassName("lock")
+  closeMenu();
 });
 
-if (modalNoButton) {
-  modalNoButton.addEventListener("click", closeModal);
-}
-
-if (modalNoButton) {
-  modalNoButton.addEventListener("click", closeBurger);
-}
-
-function closeBurger() {
-  if (modal) {
-    modal.classList.remove('open');
-  }
-  toggleButton.classList.remove('open');
-}
-
-function closeModal() {
-  if (modal) {
-    modal.classList.remove("open");
-  }
+function closeMenu() {
   backdrop.classList.remove("open");
 }
-
 
 toggleButton.addEventListener("click", function() {
   mobileNav.classList.add("open");
