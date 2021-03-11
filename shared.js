21b62ec7d2f55322ea6112497b8108a1d17b0995
Alexandr Melnikov <sasha.mel.aa@gmail.com>
2021-03-11 10:48:31 +0300

    bugfix for modal

--- shared.js
+++ shared.js
@@ -1,15 +1,15 @@
 var backdrop = document.querySelector(".backdrop");
 var toggleButton = document.querySelector(".main-burger");
 var mobileNav = document.querySelector(".mobile-nav");
-// var body = document.querySelector("body")
+var body = document.querySelector("body")
 
 backdrop.addEventListener("click", function() {
   mobileNav.classList.remove("open");
   mobileNav.getElementsByClassName('open')
   toggleButton.classList.remove("open");
   toggleButton.getElementsByClassName('open')
-  // body.classList.remove("lock")
-  // body.getElementsByClassName("lock")
+  body.classList.remove("lock")
+  body.getElementsByTagName("lock")
   closeMenu();
 });
 
@@ -21,4 +21,5 @@ toggleButton.addEventListener("click", function() {
   mobileNav.classList.add("open");
   backdrop.classList.add("open");
   toggleButton.classList.add("open");
+  body.classList.add("lock");
 });
