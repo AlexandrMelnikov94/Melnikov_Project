4d6b60bdb55233ad164ea7542f7701c9fb4b54de
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2021-04-06 13:05:23 +0300

    js refactor

--- shared.js
+++ shared.js
@@ -1,29 +1,29 @@
-var backdrop = document.querySelector(".backdrop");
-var toggleButton = document.querySelector(".main-burger");
-var mobileNav = document.querySelector(".mobile-nav");
-var body = document.querySelector("body")
-var acc = document.getElementsByClassName("accordion");
+var backdrop = document.querySelector('.backdrop');
+var toggleButton = document.querySelector('.main-burger');
+var mobileNav = document.querySelector('.mobile-nav');
+var body = document.querySelector('body')
+var acc = document.getElementsByClassName('accordion');
 var i;
 
-backdrop.addEventListener("click", function() {
-  mobileNav.classList.remove("open");
+backdrop.addEventListener('click', function() {
+  mobileNav.classList.remove('open');
   mobileNav.getElementsByClassName('open')
-  toggleButton.classList.remove("open");
+  toggleButton.classList.remove('open');
   toggleButton.getElementsByClassName('open')
-  body.classList.remove("lock")
-  body.getElementsByTagName("lock")
+  body.classList.remove('lock')
+  body.getElementsByTagName('lock')
   closeMenu();
 });
 
 function closeMenu() {
-  backdrop.classList.remove("open");
+  backdrop.classList.remove('open');
 }
 
-toggleButton.addEventListener("click", function() {
-  mobileNav.classList.add("open");
-  backdrop.classList.add("open");
-  toggleButton.classList.add("open");
-  body.classList.add("lock");
+toggleButton.addEventListener('click', function() {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+  toggleButton.classList.add('open');
+  body.classList.add('lock');
 });
 
 var smoothJumpUP = function () {
@@ -43,13 +43,13 @@ window.onscroll = function () {
 }
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+  acc[i].addEventListener('click', function() {
+    this.classList.toggle('active');
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + 'px';
     }
   });
 }
